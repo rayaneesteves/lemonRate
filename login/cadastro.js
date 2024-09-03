e4f606a06231790157875fc13e8d72b343412fa8
rayaneesteves <140505699+rayaneesteves@users.noreply.github.com>
2024-09-03 14:21:42 -0300

    atualização

---
+++ login/cadastro.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert('As senhas não coincidem.');
+        return;
+    }
+
+    alert(`Usuário: ${username}\nEmail: ${email}`);
+    // Redirecionar para a homepage
+    window.location.href = 'homepage.html';
+});
