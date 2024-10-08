1355834d94675b22d7e82443fbc7950061b94c43
MatheusMansur1307 <140506933+MatheusMansur1307@users.noreply.github.com>
2024-10-08 15:03:09 -0300

    adicionandoFilmesPopulares
    colocamos os filmes mais populares na homepage.

---
+++ login/homepage.js
@@ -0,0 +1,45 @@
+const apiKey = 'ef0bda0322a73c50aaa0ed16de544460'; // Coloque sua chave de API do TMDB aqui
+const movieContainer = document.getElementById('movies');
+
+async function fetchPopularMovies() {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`);
+  const data = await response.json();
+  
+  // Pegando apenas os 5 primeiros filmes
+  const popularMovies = data.results.slice(0, 9);
+  displayMovies(popularMovies);
+}
+
+function displayMovies(movies) {
+  movies.forEach(movie => {
+    if (movie.poster_path) {
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie');
+
+      // Criando o link que levará à página de avaliação
+      const movieLink = document.createElement('a');
+      movieLink.href = `avaliarFilme.html?movieId=${movie.id}`; // Adiciona o ID do filme como parâmetro na URL
+
+      const moviePoster = document.createElement('img');
+      moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+      moviePoster.alt = `${movie.title} Poster`;
+
+      const movieTitle = document.createElement('div');
+      movieTitle.classList.add('movie-title');
+      movieTitle.textContent = movie.title;
+
+      // Colocando o pôster dentro do link
+      movieLink.appendChild(moviePoster);
+
+      // Adicionando o link e o título dentro do elemento do filme
+      movieElement.appendChild(movieLink);
+      movieElement.appendChild(movieTitle);
+
+      // Adicionando o filme ao container
+      movieContainer.appendChild(movieElement);
+    }
+  });
+}
+
+// Chama a função para buscar filmes populares ao carregar a página
+fetchPopularMovies();
