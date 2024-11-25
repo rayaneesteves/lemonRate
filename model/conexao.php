584ae40e2ce031fd8d4d79aff34404e388e49020
MatheusMansur1307 <matheusmansur1307@gmail.com>
2024-11-25 11:44:15 -0300

    eu
    eu

--- model/conexao.php
+++ model/conexao.php
@@ -1,6 +1,6 @@
 <?php
 // Configurações do banco de dados
-$host = "52.233.90.226";
+$host = "127.0.0.1";
 $username = "lemonrate";
 $password = "U_FY%jd$J~";
 $database = "lemonrate_db";
@@ -10,11 +10,8 @@ $conn = new mysqli($host, $username, $password, $database);
 
 // Verificando a conexão
 if ($conn->connect_error) {
-    "<script>
-    alert('deu erro')
-    </script>";
+   
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-echo "Conexão estabelecida com sucesso!";
 ?>
--- model/insert.php
+++ model/insert.php
@@ -1,42 +1,42 @@
 <?php
-// Ativar a exibição de erros para depuração
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    include_once "conexao.php";
 
-try {
+
     // Ajuste o caminho para o arquivo de conexão corretamente
-    include_once "../model/conexao.php";
 
     // Receber os dados vindos do formulário
     $nome = $_POST["username"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    // Hash da senha para maior segurança
-    $senha_hashed = password_hash($senha, PASSWORD_DEFAULT);
-
     // Query SQL usando prepared statement
-    $sql = "INSERT INTO usuário (nome, email, senha, status) VALUES (:nome, :email, :senha, '1')";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':senha', $senha_hashed);
+    $sql = "INSERT INTO usuário (nome, email, senha, status) VALUES ('$nome', '$email', '$senha', '1')";
 
-    // Executa a query e verifica se foi bem-sucedida
-    if ($stmt->execute()) {
-        echo "<script>
-                alert('Registro salvo com sucesso!');
-                window.location = 'selecionar-pessoa.php';
-              </script>";
-    } else {
-        echo "<script>
-                alert('Erro ao inserir o registro.');
-                window.history.back();
-              </script>";
-    }
-} catch (Exception $e) {
-    // Exibir o erro diretamente no navegador
-    echo 'Erro: ' . $e->getMessage();
+    try{
+
+$result = $conn-> query ($sql):
+
+   if($result === TRUE ){
+?>
+<script>
+    window.location= "../view/homepage.html";
+</script>
+<?php
+
+   }else{
+    ?>
+    <script>
+alert( " Erro ao inserir registro. ");
+window.history.back();
+    </script>
+    <?php
+   }
+
+} catch ( mysql_sql_exception) {
+?>
+<script>
+window.location= "../view/perfil.html";
+</script>
+<?php
 }
 ?>
