eae32baaadbcdba6a9c1c9a2bf671a797fe5f236
MatheusMansur1307 <matheusmansur1307@gmail.com>
2024-11-26 11:40:49 -0300

    ConexaoDeuCerto
    conexao

--- model/conexao.php
+++ model/conexao.php
@@ -2,7 +2,7 @@
 // Configurações do banco de dados
 $host = "127.0.0.1";
 $username = "lemonrate";
-$password = "U_FY%jd$J~";
+$password = 'U_FY%jd$J~';
 $database = "lemonrate_db";
 
 // Criando a conexão
--- model/insert.php
+++ model/insert.php
@@ -1,42 +1,38 @@
 <?php
-    include_once "conexao.php";
+try {
+include_once "conexao.php";
+// Ajuste o caminho para o arquivo de conexão corretamente
+
+// Receber os dados vindos do formulário
+$nome = $_POST["username"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+// Query SQL usando prepared statement
+$sql = "INSERT INTO usuário (nome, email, senha, status) VALUES ('$nome', '$email', '$senha', '1')";
 
 
-    // Ajuste o caminho para o arquivo de conexão corretamente
 
-    // Receber os dados vindos do formulário
-    $nome = $_POST["username"];
-    $email = $_POST["email"];
-    $senha = $_POST["senha"];
+    $result = $conn->query($sql);
 
-    // Query SQL usando prepared statement
-    $sql = "INSERT INTO usuário (nome, email, senha, status) VALUES ('$nome', '$email', '$senha', '1')";
-
-    try{
-
-$result = $conn-> query ($sql):
-
-   if($result === TRUE ){
+    if ($result === TRUE) {
 ?>
-<script>
-    window.location= "../view/homepage.html";
-</script>
-<?php
-
-   }else{
-    ?>
-    <script>
-alert( " Erro ao inserir registro. ");
-window.history.back();
-    </script>
+        <script>
+            window.location = "../view/homepage.html";
+        </script>
     <?php
-   }
 
-} catch ( mysql_sql_exception) {
-?>
-<script>
-window.location= "../view/perfil.html";
-</script>
+    } else {
+    ?>
+        <script>
+            alert(" Erro ao inserir registro. ");
+            window.history.back();
+        </script>
+    <?php
+    }
+} catch (exception $e) {
+    echo "". $e->getMessage();
+    ?>
 <?php
 }
 ?>
